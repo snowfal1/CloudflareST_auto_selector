d93442192a193c76b064a0ed23bbc842f31039b2
snowfal1 <133531170+snowfal1@users.noreply.github.com>
2024-04-14 03:15:04 +0800

    Update and rename config.json to config.py

---
+++ config/config.py
@@ -0,0 +1,10 @@
+'''
+1.email: Cloudflare账号的邮箱
+2.global_api_key: Cloudflare账号的全局API Key
+3.zone_id: Cloudflare账号的zone id
+'''
+
+email = "your@mail.com"
+global_api_key = "your_global_api_key"
+zone_id = "your_zone_id"
+
